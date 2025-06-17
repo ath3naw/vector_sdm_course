af45548bd966912f974004c72c79211bf61cd1e5
Athena Wang <aw9689@princeton.edu>
2025-06-17 14:50:57 +0800

    adjusting for Madagascar data

--- R/prepare_raster_data.R
+++ R/prepare_raster_data.R
@@ -6,17 +6,17 @@ library(geodata)
 
 
 
-# get extent map for our basic analyses - using outline of Kenya
+# get extent map for our basic analyses - using outline of Thailand
 
-kenya_vector <- gadm(
-  country = "KEN",
+mad_vector <- gadm(
+  country = "MDG",
   level = 0,
-  path = "data/downloads"
+  path = "data"
 ) # slow to run though not large
 
-kenya_vector
+mad_vector
 
-plot(kenya_vector)
+plot(mad_vector)
 
 # let's download some data
 
@@ -24,69 +24,76 @@ plot(kenya_vector)
 # you can explore the sizes of city using ?travel time, and change size if you wish
 travel <- travel_time(
   to = "city",
-  size = 2,
-  path = "data/downloads"
-) # 421 MB - may be slow to download
+  size = 5,
+  up=TRUE,
+  path = "data"
+) # 411 MB - may be slow to download
 
 # it's a good idea to have a look at your objects and even plot them
-
+travel
+plot(travel)
 
 # bioclimactic variables from worldclim
 # https://worldclim.org/data/bioclim.html
-bioclim_kenya <- worldclim_country(
-  country = "KEN",
+bioclim_mad <- worldclim_country(
+  country = "MDG",
   var = "bio",
   res = 0.5,
-  path = "data/downloads"
+  path = "data"
 )
 
 # have a look at this object first
 # Do you think you don't want to plot all layers at once?
+bioclim_mad
+# has 19 layers
 
 # process these data to match our extent
 
 # generate mask of our area of interest so we can use it to process other data into this shape
-kenya_mask <- bioclim_kenya[[1]] %>%
-  mask(kenya_vector) * 0 + 1
+# %>% mask(mad_vector) applies the mask() function to the raster layer, using mad_vector
+mad_mask <- bioclim_mad[[1]] %>%
+  mask(mad_vector) * 0 + 1 # sets all non-NA values to 1, NA values to NA still
 
-plot(kenya_mask)
+plot(mad_mask)
 
 
 # process bioclim into shape
-bc_kenya <- mask(
-  x = bioclim_kenya,
-  mask = kenya_mask
+bc_mad <- mask(
+  x = bioclim_mad,
+  mask = mad_mask
 )
+
+# we have many layers (19)
+plot(bc_mad)
 
 # process travel data
 # this is a very large raster, we speed up the operation by cropping then masking
 # try looking at the result of only cropping to understand why both steps are needed
-travel_kenya <- crop(
+travel_mad <- crop(
   x = travel,
-  y = kenya_mask
+  y = mad_mask
 ) %>%
   mask(
-    mask = kenya_mask
+    mask = mad_mask
   )
 
 # let's have a look at it
 
-
-
+travel_mad
+# ok extent cropped
+plot(travel_mad)
 
 # now let's create a bias layer out of our travel data
 # it should scale from 0-1 where 0 is hard to get to and 1 is easy
 
-plot(travel_kenya)
-
-rescale_travel <- travel_kenya
+rescale_travel <- travel_mad
 
 rescale_travel[] <- rescale_travel[]/ max(rescale_travel[], na.rm = TRUE)
 
 rescale_travel[] <- 1 - rescale_travel[]
 
 plot(rescale_travel)
-
+# yellow is easy to get to, blue is hard to get to (now in proportions)
 
 
 # now let's write our key rasters to disk
@@ -94,13 +101,13 @@ plot(rescale_travel)
 # once those objects are saved in that location, they will be re-read from disk
 
 terra::writeRaster(
-  x = kenya_mask,
-  filename = "data/grids/kenya_mask.tif"
+  x = mad_mask,
+  filename = "data/grids/mad_mask.tif"
 )
 
 terra::writeRaster(
-  x = bc_kenya,
-  filename = "data/grids/bc_kenya.tif"
+  x = bc_mad,
+  filename = "data/grids/bc_mad.tif"
 )
 
 terra::writeRaster(
@@ -148,8 +155,8 @@ terra::writeRaster(
 # BIO19 = Precipitation of Coldest Quarter
 
 
-#plot(bc_kenya[[1:10]])
-#plot(bc_kenya[[11:19]])
+plot(bc_mad[[1:10]])
+plot(bc_mad[[11:19]])
 # 1, 5, 6, 8, 9, 10, 11 + 1; 12, 13, 14, 16, 17, 18, 19 -1
 # 2, 7, + 1
 # 3, 4, -1
